ff3afb30554db78961507559d86c819f7b5637b7
hippieperm <leess0133@gmail.com>
2025-03-03 15:46:38 +0900

    ✨ 카드 보드에 탭 이벤트 핸들링 추가

--- lib/src/card_boards.dart
+++ lib/src/card_boards.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'card.dart';
 
-class CardBoards extends StatelessWidget {
+class CardBoards extends StatefulWidget {
   CardBoards({super.key});
 
+  @override
+  State<CardBoards> createState() => _CardBoardsState();
+}
+
+class _CardBoardsState extends State<CardBoards> {
   List<int> cards = [1, 5, 2, 6, 3, 4, 3, 2, 6, 1, 4, 5];
+
+  void onTapCard(int cardIndex) {
+    print('$cardIndex 번째 카드를 선택하셨습니다.');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +26,7 @@ class CardBoards extends StatelessWidget {
           for (var i = 0; i < cards.length; i++)
             CardWidget(
               cardNumber: cards[i],
+              onTap: () => onTapCard(i),
             ),
         ],
       ),
